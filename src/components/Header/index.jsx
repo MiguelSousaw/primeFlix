8a7a9474aab1171aba78ec11a71b9ad1aa999e93
Miguel De Sousa Lima <miguellima4777@gmail.com>
2025-05-22 08:09:17 -0300

    corrigindo header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Header(){
     return (
         <header>
-            <Link to="/Home" className='logo'>Prime Flix</Link>
+            <Link to="/" className='logo'>Prime Flix</Link>
             <Link to="/Favoritos" className='favoritos'>Meus filmes</Link>
         </header>
     );
